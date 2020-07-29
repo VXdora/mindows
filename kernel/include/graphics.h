315ef39ccb88abb2f31e6ebd2a0a6caec0f47423
VXdora <naoki5336@live.jp>
2020-07-29 15:54:32 +0900

    [New] 各ソースコードのヘッダを追加

---
+++ kernel/include/graphics.h
@@ -0,0 +1,42 @@
+/**********************************************
+ *      @file   include/graphics.h
+ *
+ *      @brief  graphicsの定義
+ *
+ *      @author vxdora
+ *
+ *      @update mindows02b [New]
+ *********************************************/
+
+#ifndef __GRAPHICS__
+#define __GRAPHICS__
+
+#define GRAPHICS_INFO 0x100000
+
+typedef struct GraphicsInfo {
+    unsigned long *vram;
+    unsigned short int width;
+    unsigned short int height;
+} GraphicsInfo;
+
+// init.c
+void InitGraphics(void);
+
+// simple.c
+void DrawLine(
+        unsigned int sx, unsigned int sy,
+        signed int width, signed int height,
+        unsigned int thick, unsigned int color);
+void DrawRectangle(
+        unsigned int sx, unsigned int sy,
+        unsigned int width, unsigned int height,
+        unsigned int color);
+void DrawRectangleVoid(
+        unsigned int sx, unsigned int sy,
+        unsigned int width, unsigned int height,
+        unsigned int thick, unsigned int color);
+void DrawCircle(
+        unsigned int cx, unsigned int cy,
+        unsigned int r, unsigned int color);
+#endif  // __GRAPHICS__
+
