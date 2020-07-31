4a9e5f3f37d86c7b41dd40d95ec88f30598a5d3c
VXdora <naoki5336@live.jp>
2020-07-31 15:04:21 +0900

    [New] 文字列操作関連の機能を追加

--- kernel/include/util.h
+++ kernel/include/util.h
@@ -18,5 +18,40 @@ unsigned char isalpha(unsigned char ch);
 unsigned char isdigit(unsigned char ch);
 unsigned char isalnum(unsigned char ch);
 
+// string.c
+void *memset(void *s, unsigned char c, unsigned int n);
+unsigned int strlen(unsigned char *s);
+int strcmp(unsigned char *s, unsigned char *t);
+int strncmp(unsigned char *s, unsigned char *t, unsigned int n);
+unsigned int strcpy(unsigned char *s, unsigned char *t);
+
+// stdio.h
+#define TYPE_UCHAR      1
+#define TYPE_UINT       2
+#define TYPE_ULLINT     3
+#define TYPE_UINTX      4
+#define TYPE_ULLINTX    5
+#define TYPE_SCHAR      11
+#define TYPE_SINT       12
+#define TYPE_SLLINT     13
+#define TYPE_SINTX      13
+#define TYPE_SLLINTX    14
+
+typedef union {
+    unsigned char uc;
+    unsigned int ud;
+    unsigned long long int ulld;
+    signed char sc;
+    signed int sd;
+    signed long long int slld;
+} ATYPE;
+
+void value2str(
+        ATYPE val,
+        unsigned char *buf,
+        unsigned char type, unsigned char mf, unsigned char zf, unsigned char dig);
+unsigned int sprintf(unsigned char *s, const unsigned char *t, ...);
+
+
 #endif  // __UTIL__
 
