9df2a1f953201a40d5747d41ea3158963b0a7a99
VXdora <naoki5336@live.jp>
2020-07-31 12:46:23 +0900

    [New] 文字列操作関数の追加

---
+++ kernel/include/util.h
@@ -0,0 +1,22 @@
+/*************************************************
+ *      @file   include/util.h
+ *
+ *      @brief  utilの定義
+ *
+ *      @author vxdora
+ *
+ *      @update mindows02d [New]
+ ************************************************/
+
+#ifndef __UTIL__
+#define __UTIL__
+
+// ctype.c
+unsigned char isupper(unsigned char ch);
+unsigned char islower(unsigned char ch);
+unsigned char isalpha(unsigned char ch);
+unsigned char isdigit(unsigned char ch);
+unsigned char isalnum(unsigned char ch);
+
+#endif  // __UTIL__
+
