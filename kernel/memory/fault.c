71d97779684f7d3ad818fd0c23c8ffcf3044b22f
VXdora <naoki5336@live.jp>
2020-08-01 10:31:20 +0900

    [Fix] ハードウェア割り込み機能を追加

--- kernel/memory/fault.c
+++ kernel/memory/fault.c
@@ -5,7 +5,7 @@
  *
  *      @update vxdora
  *
- *      @update mindows03b [New]
+ *      @update mindows03c [Fix]
  ********************************************/
 
 #include <memory.h>
@@ -13,7 +13,124 @@
 
 extern GraphicsInfo graphicsInfo;
 
+//
+//      EmptyHandler
+//      @arg    :   なし
+//      @return :   なし
+//      @brief  :   意図しない例外の捕捉
+//      @author :   vxdora
+//      @update :   mindows03c [Fix]
+//
 void EmptyHandler(void) {
+	DrawRectangle(3, 3, 150, 40, 0x000000);
     DrawString(5, 5, (unsigned char *)"Empty Handler", 0xFFFFFF);
+    while (1);
+}
+
+void DeHandler(void) {
+	DrawRectangle(3, 3, 150, 40, 0x000000);
+    DrawString(5, 5, (unsigned char *)"DE Error", 0xFFFFFF);
+    while (1);
+}
+
+void DbHandler(void) {
+	DrawRectangle(3, 3, 150, 40, 0x000000);
+    DrawString(5, 5, (unsigned char *)"DB Error", 0xFFFFFF);
+    while (1);
+}
+
+void NmiHandler(void) {
+	DrawRectangle(3, 3, 150, 40, 0x000000);
+    DrawString(5, 5, (unsigned char *)"NMI Error", 0xFFFFFF);
+    while (1);
+}
+
+void BpHandler(void) {
+	DrawRectangle(3, 3, 150, 40, 0x000000);
+    DrawString(5, 5, (unsigned char *)"BP Error", 0xFFFFFF);
+    while (1);
+}
+
+void OfHandler(void) {
+	DrawRectangle(3, 3, 150, 40, 0x000000);
+    DrawString(5, 5, (unsigned char *)"OF Error", 0xFFFFFF);
+    while (1);
+}
+
+void BrHandler(void) {
+	DrawRectangle(3, 3, 150, 40, 0x000000);
+    DrawString(5, 5, (unsigned char *)"BR Error", 0xFFFFFF);
+    while (1);
+}
+
+void UdHandler(void) {
+	DrawRectangle(3, 3, 150, 40, 0x000000);
+    DrawString(5, 5, (unsigned char *)"UD Error", 0xFFFFFF);
+    while (1);
+}
+
+void NmHandler(void) {
+	DrawRectangle(3, 3, 150, 40, 0x000000);
+    DrawString(5, 5, (unsigned char *)"NM Error", 0xFFFFFF);
+    while (1);
+}
+
+void DfHandler(void) {
+	DrawRectangle(3, 3, 150, 40, 0x000000);
+    DrawString(5, 5, (unsigned char *)"DF Error", 0xFFFFFF);
+    while (1);
+}
+
+void TsHandler(void) {
+	DrawRectangle(3, 3, 150, 40, 0x000000);
+    DrawString(5, 5, (unsigned char *)"TS Error", 0xFFFFFF);
+    while (1);
+}
+
+void NpHandler(void) {
+	DrawRectangle(3, 3, 150, 40, 0x000000);
+    DrawString(5, 5, (unsigned char *)"NP Error", 0xFFFFFF);
+    while (1);
+}
+
+void SsHandler(void) {
+	DrawRectangle(3, 3, 150, 40, 0x000000);
+    DrawString(5, 5, (unsigned char *)"SS Error", 0xFFFFFF);
+    while (1);
+}
+
+void GpHandler(void) {
+	DrawRectangle(3, 3, 150, 40, 0x000000);
+    DrawString(5, 5, (unsigned char *)"GP Error", 0xFFFFFF);
+    while (1);
+}
+
+void PfHandler(void) {
+	DrawRectangle(3, 3, 150, 40, 0x000000);
+    DrawString(5, 5, (unsigned char *)"PF Error", 0xFFFFFF);
+    while (1);
+}
+
+void MfHandler(void) {
+	DrawRectangle(3, 3, 150, 40, 0x000000);
+    DrawString(5, 5, (unsigned char *)"MF Error", 0xFFFFFF);
+    while (1);
+}
+
+void AcHandler(void) {
+	DrawRectangle(3, 3, 150, 40, 0x000000);
+    DrawString(5, 5, (unsigned char *)"AC Error", 0xFFFFFF);
+    while (1);
+}
+
+void McHandler(void) {
+	DrawRectangle(3, 3, 150, 40, 0x000000);
+    DrawString(5, 5, (unsigned char *)"MC Error", 0xFFFFFF);
+    while (1);
+}
+
+void XmHandler(void) {
+	DrawRectangle(3, 3, 150, 40, 0x000000);
+    DrawString(5, 5, (unsigned char *)"XM Error", 0xFFFFFF);
     while (1);
 }
--- kernel/memory/gdtidt.c
+++ kernel/memory/gdtidt.c
@@ -5,7 +5,7 @@
  *
  *      @author vxdora
  *
- *      @update mindows03a [Fix]
+ *      @update mindows03c [Fix]
  *********************************************/
 
 #include <memory.h>
@@ -34,13 +34,32 @@ void InitGdt(void) {
 //      @return :   なし
 //      @brief  ;   IDTの初期化
 //      @author :   vxdora
-//      @update :   mindows03b [New]
+//      @update :   mindows03c [Fix]
 //
 void InitIdt(void) {
     unsigned int i;
     for (i = 0; i < 256; i++) {
         IDT[i] = MakeGateDescriptor((unsigned long long)AsmEmptyHandler);
     }
+	IDT[0x00] = MakeGateDescriptor((unsigned long long)AsmDeHandler);
+    IDT[0x01] = MakeGateDescriptor((unsigned long long)AsmDbHandler);
+    IDT[0x02] = MakeGateDescriptor((unsigned long long)AsmNmiHandler);
+    IDT[0x03] = MakeGateDescriptor((unsigned long long)AsmBpHandler);
+    IDT[0x04] = MakeGateDescriptor((unsigned long long)AsmOfHandler);
+    IDT[0x05] = MakeGateDescriptor((unsigned long long)AsmBrHandler);
+    IDT[0x06] = MakeGateDescriptor((unsigned long long)AsmUdHandler);
+    IDT[0x07] = MakeGateDescriptor((unsigned long long)AsmNmHandler);
+    IDT[0x08] = MakeGateDescriptor((unsigned long long)AsmDfHandler);
+    IDT[0x0A] = MakeGateDescriptor((unsigned long long)AsmTsHandler);
+    IDT[0x0B] = MakeGateDescriptor((unsigned long long)AsmNpHandler);
+    IDT[0x0C] = MakeGateDescriptor((unsigned long long)AsmSsHandler);
+    IDT[0x0D] = MakeGateDescriptor((unsigned long long)AsmGpHandler);
+    IDT[0x0E] = MakeGateDescriptor((unsigned long long)AsmPfHandler);
+    IDT[0x10] = MakeGateDescriptor((unsigned long long)AsmMfHandler);
+    IDT[0x11] = MakeGateDescriptor((unsigned long long)AsmAcHandler);
+    IDT[0x12] = MakeGateDescriptor((unsigned long long)AsmMcHandler);
+    IDT[0x13] = MakeGateDescriptor((unsigned long long)AsmXmHandler);
+	
     LoadIdt();
 }
 
@@ -50,7 +69,7 @@ void InitIdt(void) {
 //      @return :   アドレスを基に作成されたGate Descriptor
 //      @brief  :   Gate Descriptorの作成
 //      @author :   vxdora
-//      @update :   mindows03b [New]
+//      @update :   mindows03b
 //
 GATE_DESCRIPTOR MakeGateDescriptor(unsigned long long addr) {
     GATE_DESCRIPTOR gateDesc;
